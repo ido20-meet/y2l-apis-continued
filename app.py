4f6e7691427065e88bdc6fccf52ce36c4bfe1077
myname21-meet <myname21@meet.mit.edu>
2019-12-19 18:52:33 +0200

    blaaaaaaaaa

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
+import requests, json
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/study_image', methods = ['POST'])
 def study_image():
-    
-    image_url = request.form['url-input']
-    # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
-    # and read the result, make sure that you read and understand the
-    # example we covered in the slides! 
+	
+	image_url = request.form['url-input']
+	# At this point you have the image_url value from the front end
+	# Your job now is to send this information to the Clarifai API
+	# and read the result, make sure that you read and understand the
+	# example we covered in the slides! 
 
-    # YOUR CODE HERE!
-    
-    return render_template('home.html', results="No results yet :(")
+	# YOUR CODE HERE!
+	headers = {'Authorization': '6e105891d58d4f4fb51758c999a89932'}
+	api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+	data ={"inputs": [
+			{
+			"data": {
+				"image": {
+					"url": image_url
+					}
+				}
+			}
+		]}
+
+	response = requests.post(api_url, headers=headers, data=json.dumps(data))
+	return render_template('home.html', results=response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
